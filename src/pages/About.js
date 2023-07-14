ad26ea3fdd29478b129e239f42f8c9b93518f0fa
shreya <gargshreya1234@gmail.com>
2023-07-14 14:53:14 +0530

    restuarant website

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const About = () => {
+  return (
+    <div>
+        <Layout>
+        <h1>About page</h1>
+        </Layout>
+      
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/Pagenotfound.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const Pagenotfound = () => {
+  return (
+    <div>
+      <Layout>
+            <h1>Page Not Found</h1>
+      </Layout>
+    </div>
+  )
+}
+
+export default Pagenotfound
